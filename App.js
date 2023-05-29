cc39a33b6d97d266a6e8472e5caa01fb8ecbb359
ameeraazli <ameera.azli@ytl.com>
2023-05-29 14:41:01 +0800

    Navigating Programmatically Between Screens - react screens, previous commit is how to do for react components

--- App.js
+++ App.js
@@ -16,15 +16,23 @@ const BottomTabs = createBottomTabNavigator();
 
 function ExpensesOverview() {
   return (
-    <BottomTabs.Navigator screenOptions={{
-      headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
-      headerTintColor: 'white',
-      tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
-      tabBarActiveTintColor: GlobalStyles.colors.accent500,
-      headerRight: ({ tintColor }) => (
-        <IconButton icon="add" size={24} color={tintColor} onPress={() => {}} />
-      ),
-    }}>
+    <BottomTabs.Navigator screenOptions={({ navigation }) => ({
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        headerTintColor: 'white',
+        tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+        headerRight: ({ tintColor }) => (
+          <IconButton
+            icon="add"
+            size={24}
+            color={tintColor}
+            onPress={() => {
+              navigation.navigate('ManageExpense');
+            }}
+          />
+        ),
+      })}
+    >
       <BottomTabs.Screen
         name="RecentExpenses"
         component={RecentExpenses}
